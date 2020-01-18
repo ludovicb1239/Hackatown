1a729ab9646bade1916dcfd9ba6434a503572cb2
RobotGuy2 <40904101+RobotGuy2@users.noreply.github.com>
2020-01-18 15:45:21 -0500

    Added all basics files
    yes

---
+++ simpleeat/index.js
@@ -0,0 +1,10 @@
+function SendLogInfo() {
+  window.location.href = "file:///D:/e175169/Bureau/simpleeat/CreateNew.html";
+}
+function CreateNewAccount() {
+  if (Password. != Password2.value){
+    alert("Passwords arent identical");
+  }else{
+    window.location.href = "file:///D:/e175169/Bureau/simpleeat/LogIn.html";
+  }
+}
