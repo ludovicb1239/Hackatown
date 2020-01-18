403e088bddb0922a320283d6cef2fe709a78297a
Ludovic Bouchard <ludovicb1239@gmail.com>
2020-01-18 18:35:00 -0500

    menu
    updated menu

--- simpleeat/index.js
+++ simpleeat/index.js
@@ -2,9 +2,9 @@ function SendLogInfo() {
   window.location.href = "file:///D:/e175169/Bureau/simpleeat/CreateNew.html";
 }
 function CreateNewAccount() {
-  if (Password. != Password2.value){
+  /*if (Password. != Password2.value){
     alert("Passwords arent identical");
   }else{
     window.location.href = "file:///D:/e175169/Bureau/simpleeat/LogIn.html";
-  }
+  }*/
 }
