ada2836fe26eb0a97bf70dc905b97c0f758f285a
RobotGuy2 <40904101+RobotGuy2@users.noreply.github.com>
2020-01-18 21:50:40 -0500

    Add files via upload

--- simpleeat/index.js
+++ simpleeat/index.js
@@ -1,10 +1,30 @@
+function SendMenu(){
+  window.location.href = "file:///D:/e175169/Bureau/simpleeat/Menu.html";
+}
+
+function SendMenu0() {
+  window.location.href = "file:///D:/e175169/Bureau/simpleeat/Menu0.html";
+}
+function goDiner(){
+  window.location.href = "file:///D:/e175169/Bureau/simpleeat/DinerSouper.html";
+}
+function GoLogIn(){
+  window.location.href = "file:///D:/e175169/Bureau/simpleeat/LogIn.html";
+}
+
 function SendLogInfo() {
   window.location.href = "file:///D:/e175169/Bureau/simpleeat/CreateNew.html";
 }
+function goToGroceryList(){
+  window.location.href = "file:///D:/e175169/Bureau/simpleeat/GroceryList.html";
+}
 function CreateNewAccount() {
-  /*if (Password. != Password2.value){
-    alert("Passwords arent identical");
-  }else{
-    window.location.href = "file:///D:/e175169/Bureau/simpleeat/LogIn.html";
-  }*/
+  var firstInput = document.getElementById("password").value;
+  var secondInput = document.getElementById("password2").value;
+
+  if (firstInput === secondInput) {
+    window.location.href = "file:///D:/e175169/Bureau/simpleeat/Menu.html";
+  } else {
+    alert('Noooo');
+  }
 }
